d6dc0d2e2f569f52c6e6fb9ce442f4c00a8d70da
Aqil Marwan <64199638+aqilmarwan@users.noreply.github.com>
2026-09-25 09:51:42 +0800

    Add: rotate-list - submission-0

---
+++ Data Structures & Algorithms/rotate-list/submission-0.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        # [1, 2, 3, 4, 5, 6 ], k = 2 -> [5, 6, 1, 2, 3, 4]
+
+        # (length - k - 1)
+        # (6 - 2 - 1) = 3
+
+        # [1 -> 2 -> 3 -> 4 -> NULL -> 5 -> 6]
+
+        if not head:
+            return head
+        
+        length, tail = 1, head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head
+
+        for i in range(length - k - 1):
+            cur = cur.next
+        
+        head = cur.next
+        cur.next = None
+        return head
+            
